6482d74d39db96281a01fe4139aaf7c1aa91f907
Blake Li <blake@misfit.com>
2016-02-17 23:49:37 +0800

    增加后缀选项

--- example.js
+++ example.js
@@ -12,34 +12,34 @@ const app = koa();
 //app.use(serve(__dirname + '/asset'));
 var url = '/build'
 app.use(serve('test/fixtures', {
-  prefix: url,
-  //format:false  
-  //defer: true
+    prefix: url,
+    suffix: ".html",
+    //format:false
+    defer: true
 }));
 app.use(serve(__dirname + '/asset'));
 
 
 app.use(function *(next) {
-  yield next;
-  if ('/' == this.path) {
-    this.body = 'Try `GET /package.json`';
-  }
+    yield next;
+    if ('/' == this.path) {
+        this.body = 'Try `GET /package.json`';
+    }
 })
 
 app.use(function *(next) {
-  yield next;
-  if ('/asd.txt' == this.path) {
-    this.body = 'Try `GET /package.json`';
-  }
+    yield next;
+    if ('/asd.txt' == this.path) {
+        this.body = 'Try `GET /package.json`';
+    }
 })
 
 app.use(function*(next) {
-  yield next;
-  if ('/aaa' == this.path) {
-    this.body = 'Try `GET /package.json`';
-  }
+    yield next;
+    if ('/aaa' == this.path) {
+        this.body = 'Try `GET /package.json`';
+    }
 })
-
 app.listen(3000);
 
 console.log('listening on port 3000');
--- index.js
+++ index.js
@@ -24,51 +24,52 @@ module.exports = serve;
  * @api public
  */
 
-function serve (root, opts) {
-  opts = opts || {};
+function serve(root, opts) {
+    opts = opts || {};
 
-  assert(root, 'root directory is required to serve files');
+    assert(root, 'root directory is required to serve files');
 
-  // options
-  debug('static "%s" %j', root, opts);
-  opts.root = resolve(root);
-  if (opts.index !== false) opts.index = opts.index || 'index.html';
-  var url = opts.prefix || opts.url || "/"
-  //prefix url
-  if (url.substr(0, 1) !== "/")
-    url = "/" + url
-  if (url.substr(-1) !== "/")
-    url += "/"
-  if (!opts.defer) {
-    return function *serve (next) {
-      if (this.method != 'HEAD' && this.method != 'GET') return;
-      var pathUrl = this.path
-      if (url.replace(/^\//, "").replace(/\/$/, "") === pathUrl.replace(/^\//, "").replace(/\/$/, "")) {
-        yield send(this, "/", opts);
-      } else if (pathUrl.substr(0, url.length) === url) {
-        console.log(pathUrl)
-        console.log("url:" + url)
-        var sub = pathUrl.replace(url, "")
-        yield send(this, sub, opts)
-      } else {
-        yield* next
-      }
+    // options
+    debug('static "%s" %j', root, opts);
+    opts.root = resolve(root);
+    if (opts.index !== false) opts.index = opts.index || 'index.html';
+    var url = opts.prefix || opts.url || "/"
+    //prefix url
+    if (url.substr(0, 1) !== "/")
+        url = "/" + url
+    if (url.substr(-1) !== "/")
+        url += "/"
+    if (!opts.defer) {
+        return function *serve(next) {
+            if (this.method != 'HEAD' && this.method != 'GET') return;
+            var pathUrl = this.path
+            if (url.replace(/^\//, "").replace(/\/$/, "") === pathUrl.replace(/^\//, "").replace(/\/$/, "")) {
+                yield send(this, "/", opts);
+            } else if (pathUrl.substr(0, url.length) === url) {
+                console.log(pathUrl)
+                console.log("url:" + url)
+                var sub = pathUrl.replace(url, "")
+                //如果有后缀 则添加后缀
+                yield send(this, sub + (opts.suffix || ""), opts)
+            } else {
+                yield* next
+            }
+        }
     }
-  }
 
-  return function *serve (next) {
-    yield* next;
+    return function *serve(next) {
+        yield* next;
 
-    if (this.method != 'HEAD' && this.method != 'GET') return;
-    // response is already handled
-    if (this.body != null || this.status != 404) return;
+        if (this.method != 'HEAD' && this.method != 'GET') return;
+        // response is already handled
+        if (this.body != null || this.status != 404) return;
 
-    var pathUrl = this.path
-    if (url.replace(/^\//, "").replace(/\/$/, "") === pathUrl.replace(/^\//, "").replace(/\/$/, "")) {
-      yield send(this, "/", opts);
-    } else if (pathUrl.substr(0, url.length) === url) {
-      var sub = pathUrl.replace(url, "")
-      yield send(this, sub, opts)
+        var pathUrl = this.path
+        if (url.replace(/^\//, "").replace(/\/$/, "") === pathUrl.replace(/^\//, "").replace(/\/$/, "")) {
+            yield send(this, "/", opts);
+        } else if (pathUrl.substr(0, url.length) === url) {
+            var sub = pathUrl.replace(url, "")
+            yield send(this, sub + (opts.suffix || ""), opts)
+        }
     }
-  }
 }
